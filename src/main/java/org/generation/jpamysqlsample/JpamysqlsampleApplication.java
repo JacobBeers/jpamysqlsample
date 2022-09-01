2bd3f7527829a4f93c79d7a2f1052b061bd0f9fa
Ken Smith <kpsc59@umstem.edu>
2022-09-01 07:52:46 -0500

    Initial Commit

---
+++ src/main/java/org/generation/jpamysqlsample/JpamysqlsampleApplication.java
@@ -0,0 +1,13 @@
+package org.generation.jpamysqlsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpamysqlsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpamysqlsampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/generation/jpamysqlsample/service/UserService.java
@@ -0,0 +1,13 @@
+package org.generation.jpamysqlsample.service;
+
+import org.generation.jpamysqlsample.model.User;
+
+public interface UserService {
+
+    User getUser(String id);
+
+    User save(User user);
+
+    void delete(String id);
+
+}
